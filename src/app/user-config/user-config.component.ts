74c8ba3dce702dcf11b945bea5c0d0bdc0dab389
NeerajVishwasrao <neerajvishwasrao@gmail.com>
2025-02-01 20:41:44 +0530

    pegination added

--- src/app/user-config/user-config.component.ts
+++ src/app/user-config/user-config.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../constant-data';
 import { PageEvent } from '@angular/material/paginator';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { UserModelComponent } from '../models/edit-user/user-model.component';
 import { AddUserComponent } from '../models/add-user/add-user.component';
 import { DeleteUserComponent } from '../models/delete-user/delete-user.component';
@@ -11,33 +11,26 @@ import { DeleteUserComponent } from '../models/delete-user/delete-user.component
   templateUrl: './user-config.component.html',
   styleUrls: ['./user-config.component.css']
 })
-
 export class UserConfigComponent implements OnInit {
-  // Declare arrays for storing users and filtered users
-  filteredUsers: User[] = [] // Start with all users
-  allUsers: User[] = []
-  
-  // Declare variables for search and sorting
-  searchName: string = ''; // Store the search query for name
-  sortBy: string = ''; // Store the selected sorting property
-  searchEmail: string = ''; // Store the search query for email
-  
-  // Declare variables for pagination and user editing
-  name: string = "";
-  pageSize = 5; // Default page size
-  currentPage = 0; // Default current page
-  pagedUsers: any[] = [];
-  editedUser: User | undefined
-  editedUserIndex: number = 0
-  
-  constructor(public dialog: MatDialog) { }
+  filteredUsers: User[] = [];
+  allUsers: User[] = [];
+
+  // Pagination variables
+  pageSize = 10; // Default page size
+  pageIndex = 0; // Default current page
+  pagedUsers: User[] = [];
+
+  searchName: string = '';
+  searchEmail: string = '';
+  sortBy: string = '';
+
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit() {
-    this.getUsers()
+    this.getUsers();
   }
 
-  // Function to load users into allUsers array
-  getUsers(){
+  getUsers() {
     this.allUsers = [
       { name: "niraj", email: "niraj@123", role: "Admin" },
       { name: "ankush", email: "ankush@123", role: "User" },
@@ -49,95 +42,102 @@ export class UserConfigComponent implements OnInit {
       { name: "dinesh", email: "dinesh@123", role: "User" },
       { name: "rahul", email: "rahul@123", role: "Admin" },
       { name: "aryan", email: "aryan@123", role: "User" },
-    ]
 
-    // Initialize filteredUsers as a copy of allUsers
+      { name: "sayli", email: "sayli@123", role: "User" },
+      { name: "akshada", email: "akshada@123", role: "User" },
+      { name: "vaibhavi", email: "vaibhavi@123", role: "Admin" },
+      { name: "aditya", email: "aditya@123", role: "User" },
+    ];
+
     this.filteredUsers = [...this.allUsers];
+    this.updatePagedUsers();
   }
 
-  // Sort the users based on the selected property
-  sort() {
-    console.log("sort");
-    let property: keyof User = this.sortBy as keyof User;
-    this.filteredUsers.sort((a, b) => {
-      let valA = a[property]?.toString().toLowerCase() || '';
-      let valB = b[property]?.toString().toLowerCase() || '';
-      return valA.localeCompare(valB);
-    });
+    updatePagedUsers() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedUsers = this.filteredUsers.slice(startIndex, endIndex);
   }
 
-  // Show all users without any filter
+  handlePageEvent(event: PageEvent) {
+    
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    console.log(event.pageSize," ",event.pageIndex);
+
+    this.updatePagedUsers();
+  }
+
   showAll() {
     this.filteredUsers = [...this.allUsers];
+    this.pageIndex = 0;
+    this.updatePagedUsers();
   }
 
-  // Delete user and handle dialog for confirmation
-  deleteUser(deleteUser: User, i: number) {
-    let deleteUserModel = this.dialog.open(DeleteUserComponent, {
-      width: '30%',
-      height: '170px',
-      data: {
-        user: deleteUser, index: i
-      }
-    })
-
-    // After dialog closes, handle the deletion process
-    deleteUserModel.afterClosed().subscribe((res) => {
-      if (res.isDelete) {
-        this.allUsers.splice(res.index, 1) 
-        this.filteredUsers = this.allUsers 
-      } else {
-        console.log("not deleted");
-      }
-    })
-  }
-
-  // Open a dialog to add a new user
-  addUserPopup() {
-    let addModel = this.dialog.open(AddUserComponent, {
-      width: '30%',
-      height: '320px',
-      data: {}
-    })
-
-    // After dialog closes, add the new user to allUsers
-    addModel.afterClosed().subscribe((user) => {
-      if (user) {
-        this.allUsers.push(user) 
-        this.filteredUsers = this.allUsers 
-      }
-    })
-  }
-
-  // Function to search users based on name and email
   searchUsers() {
     this.filteredUsers = this.allUsers.filter(user =>
       (this.searchName === '' || user.name.toLowerCase().includes(this.searchName.toLowerCase())) &&
       (this.searchEmail === '' || user.email.toLowerCase().includes(this.searchEmail.toLowerCase()))
     );
+    this.pageIndex = 0;
+    this.updatePagedUsers();
   }
 
-  // Open the edit user dialog and update user data
+  sort() {
+    const property: keyof User = this.sortBy as keyof User;
+    this.filteredUsers.sort((a, b) => {
+      let valA = a[property]?.toString().toLowerCase() || '';
+      let valB = b[property]?.toString().toLowerCase() || '';
+      return valA.localeCompare(valB);
+    });
+    this.updatePagedUsers();
+  }
+
+  deleteUser(deleteUser: User, i: number) {
+    let deleteUserModel = this.dialog.open(DeleteUserComponent, {
+      width: '30%',
+      height: '170px',
+      data: { user: deleteUser, index: i }
+    });
+
+    deleteUserModel.afterClosed().subscribe((res) => {
+      if (res?.isDelete) {
+        this.allUsers.splice(res.index, 1);
+        this.filteredUsers = this.allUsers;
+        this.updatePagedUsers();
+      }
+    });
+  }
+
+  addUserPopup() {
+    let addModel = this.dialog.open(AddUserComponent, {
+      width: '30%',
+      height: '320px',
+      data: {}
+    });
+
+    addModel.afterClosed().subscribe((user) => {
+      if (user) {
+        this.allUsers.push(user);
+        this.filteredUsers = this.allUsers;
+        this.updatePagedUsers();
+      }
+    });
+  }
+
   openEditPoppup(oneuser: User, i: number) {
     let popup = this.dialog.open(UserModelComponent, {
       width: '30%',
       height: '320px',
-      data: {
-        user: oneuser, index: i
-      }
-    })
+      data: { user: oneuser, index: i }
+    });
 
-    // After dialog closes, update the user data if edited
     popup.afterClosed().subscribe((modelRes) => {
       if (modelRes) {
-        this.editedUser = modelRes.editedUser
-        this.editedUserIndex = modelRes.index
-
-        this.allUsers[modelRes.index] = modelRes.editedUser 
-        this.filteredUsers = this.allUsers 
-    }
-  })
-
-
+        this.allUsers[modelRes.index] = modelRes.editedUser;
+        this.filteredUsers = this.allUsers;
+        this.updatePagedUsers();
+      }
+    });
   }
 }
